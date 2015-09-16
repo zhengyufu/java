b6bb75e9a26b34fa00bb9c394d7fa44f1ff96442
fzy97212@sina.com <fzy97212@sina.com>
2015-09-16 20:44:44 +0800

    anno

--- fzyMvc/src/com/fzy/web/action/UserAction.java
+++ fzyMvc/src/com/fzy/web/action/UserAction.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fzy.framework.annotation.ActionAnno;
 import com.fzy.framework.mvc.ActionBase;
 import com.fzy.framework.pojo.PojoBase;
 import com.fzy.web.pojo.UserInfo;
@@ -11,12 +12,12 @@ import com.fzy.web.pojo.UserInfo;
 public class UserAction implements ActionBase {
 
 	@Override
+	@ActionAnno("login")
 	public String execute(HttpServletRequest request,PojoBase form,Map<String,String> forword) {
 
 		UserInfo userInfo = (UserInfo)form;
 		
 		String url=forword.get("success");
-
 		
 		return url;
 	}
